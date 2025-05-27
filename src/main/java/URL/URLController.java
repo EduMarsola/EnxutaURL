d2c9846dbe79660bbf5a5c5f204476c2cad77019
EduMarsola <eduimarsola@gmail.com>
2025-05-26 23:37:25 -0300

    26/05/2025 - request not working

--- src/main/java/URL/URLController.java
+++ src/main/java/URL/URLController.java
@@ -1,12 +1,16 @@
 package URL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Optional;
 import InternalFeatures.Compactor;
 
 public class URLController {
     public void teste(){
-        System.out.println("Testado");
+
     }
+
     public Optional<String> Compact(String originalURL){
         Optional<String> response;
         response = Optional.of(Compactor.compact(originalURL));
--- src/main/java/URL/URLModel.java
+++ src/main/java/URL/URLModel.java
@@ -1,17 +1,28 @@
 package URL;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping("url")
 public class URLModel {
-    URLController urlController = new URLController();
 
-    @GetMapping("/generateCompact")
+    @Autowired
+    @GetMapping("/test")
+    public String teste(){
+        return "testado";
+    }
+    @GetMapping("/generateCompact/")
     public String GenerateCompact(@RequestParam("originalURL") String originalURL){
+        URLController urlController = new URLController();
         if(urlController.Compact(originalURL).isPresent()){
             return urlController.Compact(originalURL).get();
         }
